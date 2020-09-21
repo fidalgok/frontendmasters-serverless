ab0f323fb1a0337223c09cf5bb271ca8be8d88bf
Kyle Fidalgo <fidalgok@bc-macbookpro-78abd3.bc.edu>
2020-09-21 18:20:52 -0400

    tested search params and added omdbapi call to get movie scores

---
+++ functions/movie-by-id.js
@@ -0,0 +1,15 @@
+const movies = require('../data/movies.json');
+exports.handler = async ({queryStringParameters}) => {
+    const {id} = queryStringParameters;
+    const movie = movies.find(m => m.id === id);
+    if (!movie){
+        return{
+            statusCode: 404,
+            body: 'not found',
+        }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(movie),
+    }
+}
--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,32 @@
+const {URL} = require('url');
+const fetch = require('node-fetch');
 const movies = require('../data/movies.json');
 
 exports.handler = async () => {
+    const api = new URL('https://www.omdbapi.com/');
+
+    
+    
+    const promises = movies.map((movie) => {
+        // use the movie's IMDB ID to look up details
+        api.searchParams.set('i', movie.id);
+        // add the secret api key
+        api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+        return fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            const scores = data.Ratings;
+            console.log(data)
+            return {
+                ...movie,
+                scores,
+            }
+        })
+    });
+
+    const moviesWithRatings = await Promise.all(promises);
     return {
         statusCode: 200,
-        body: JSON.stringify(movies),
+        body: JSON.stringify(moviesWithRatings),
     };
 }
